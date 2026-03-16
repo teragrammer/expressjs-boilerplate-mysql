73f1614adad84e45ad2839044e1c65f9c2e4b616
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-03-16 11:08:11 -0400

    Create interfaces for global Request and Response
    New method for response with failed methods added
    
    Formatting request object with new data and query

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,27 +1,41 @@
-import {UserInterface} from "../../interfaces/user.interface";
-import {AuthenticationTokenInterface} from "../../interfaces/authentication-token.interface";
 import {JwtExtendedPayload} from "../../models/authentication-token.model";
+import {UserRole} from "../../interfaces/user";
+import {AuthenticationToken} from "../../interfaces/authentication.token";
+
+export interface RequestCredentials {
+    jwt: JwtExtendedPayload;
+    user: () => Promise<UserRole>;
+    authentication: () => Promise<AuthenticationToken>;
+}
+
+export interface RequestSanitize {
+    body: {
+        get: (key: string, defaults?: any) => any;
+        only: (keys: string[], defaults?: Record<string, any> | undefined) => Record<string, any>;
+        numeric: (key: string, defaults?: any) => any;
+    },
+    query: {
+        get: (key: string, defaults?: any) => any;
+        only: (keys: string[], defaults?: Record<string, any> | undefined) => Record<string, any>;
+        numeric: (key: string, defaults?: any) => any;
+    },
+    data?: any,
+}
+
+export interface ResponseFailed {
+    message: (status: number, message?: string, code?: string) => any;
+    fields: (status: number, errors: any) => any;
+}
 
 declare global {
     namespace Express {
         interface Request {
-            credentials: {
-                jwt: JwtExtendedPayload;
-                user: () => Promise<UserInterface>;
-                authentication: () => Promise<AuthenticationTokenInterface>;
-            };
+            credentials: RequestCredentials;
+            sanitize: RequestSanitize;
+        }
 
-            sanitize: {
-                body: {
-                    get: (key: string, defaults?: any) => any;
-                    only: (keys: string[]) => Record<string, any>;
-                    numeric: (key: string, defaults?: any) => any;
-                },
-                query: {
-                    get: (key: string, defaults?: any) => any;
-                    numeric: (key: string, defaults?: any) => any;
-                }
-            };
+        interface Response {
+            failed: ResponseFailed;
         }
     }
 }
