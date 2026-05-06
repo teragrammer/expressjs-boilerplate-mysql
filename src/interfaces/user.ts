dc641b9b66b6a3dce02fe1ea23979c386ef74ef3
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-05-06 11:08:19 -0400

    Create new user interface with separated role details

---
+++ src/interfaces/user.ts
@@ -0,0 +1,27 @@
+export interface User {
+    id: number;
+    first_name: string;
+    middle_name: string | null;
+    last_name: string;
+    gender: string | null;
+    address: string | null;
+    phone: string | null;
+    is_phone_verified: number;
+    email: string | null;
+    is_email_verified: number;
+    role_id: number;
+    username: string | null;
+    password?: string | null;
+    status: string;
+    comments: string | null;
+    login_tries?: number;
+    failed_login_expired_at?: string | null;
+    created_at: string | null;
+    updated_at: string | null;
+}
+
+export interface UserRole extends User {
+    slug?: string;
+    is_public?: number;
+    is_bypass_authorization?: number;
+}
