8ea932c7e219555c67f3dc6a04bd56644aadf338
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-07-17 04:55:04 +0000

    feat(auth): update AuthenticationToken interface properties and export creation/update input types

--- src/modules/auth/interfaces/authentication.token.ts
+++ src/modules/auth/interfaces/authentication.token.ts
@@ -1,10 +1,15 @@
+// src/modules/auth/interfaces/authentication.token.ts
+
 export interface AuthenticationToken {
     id: number;
-    user_id: number;
-    expired_at: string | null;
+    user_id: number | null;
+    expired_at: Date | string | null;
     ip: string | null;
     browser: string | null;
     os: string | null;
-    created_at: string | null;
-    updated_at: string | null;
+    created_at: Date | string | null;
+    updated_at: Date | string | null;
 }
+
+export type CreateAuthenticationTokenInput = Omit<AuthenticationToken, "id" | "created_at" | "updated_at">;
+export type UpdateAuthenticationTokenInput = Partial<CreateAuthenticationTokenInput>;
