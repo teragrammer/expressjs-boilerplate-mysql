86fccd22566d1d6b80643b1a69d5402bd0693ffe
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-07-20 09:53:56 +0000

    feat: add user lookup method to user service

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,4 +1,5 @@
 // src/modules/users/user.service.ts
+
 import {UserRepository} from "./user.repository";
 import {CreateUserDTO, User} from "./user.interface";
 import {AppError} from "../../common/utils/errors";
@@ -7,6 +8,21 @@ import Messages from "../../common/utils/messages";
 export class UserService {
     // Dependency injection allows passing a mock repository during testing
     constructor(private userRepo = new UserRepository()) {
+    }
+
+    /**
+     * Resolves a user profile by its unique ID for authentication context mapping.
+     */
+    async findById(id: number): Promise<any> {
+        const user = await this.userRepo.findById(id);
+        if (!user) {
+            throw new AppError(
+                Messages.DATA_NOT_FOUND.message,
+                Messages.DATA_NOT_FOUND.code,
+                404
+            );
+        }
+        return user;
     }
 
     /**
