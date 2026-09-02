2cde8f7bab69eb616f212cf31d69369d07d08b85
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-09-02 09:24:47 +0000

    refactor: inject auth service into authentication controller

--- src/modules/auth/controllers/authentication.controller.ts
+++ src/modules/auth/controllers/authentication.controller.ts
@@ -2,29 +2,26 @@
 
 import {Request, Response} from "express";
 import catchAsync from "../../../common/utils/catch-async";
-import {loginSchema} from "../validations/login.schema";
-import {authService} from "../services/auth.service";
+import {AuthService} from "../services/auth.service";
+import {LoginInput} from "../interfaces/login-input.interface";
 
 export class AuthenticationController {
-    static login = catchAsync(async (req: Request, res: Response) => {
-        // Sanitize input
-        const rawData = req.sanitize.body.only([
-            "username",
-            "password",
-        ]);
+    constructor(
+        private readonly authService: AuthService,
+    ) {
+    }
 
-        // Validate input
-        const validatedData = await loginSchema.validateAsync(rawData, {abortEarly: false});
-
+    login = catchAsync(async (req: Request, res: Response) => {
         // Delegate to Business Service
-        const result = await authService.login(validatedData);
+        const result = await this.authService.login(req.sanitize.data as unknown as LoginInput);
 
         // Send HTTP response
         res.status(200).json(result);
     });
 
-    static logout = catchAsync(async (req: Request, res: Response): Promise<any> => {
-        await authService.logout(req.credentials.jwt.tid);
-        res.status(200).send();
+    logout = catchAsync(async (req: Request, res: Response) => {
+        await this.authService.logout(req.credentials.jwt.tid);
+
+        res.sendStatus(204);
     });
 }
