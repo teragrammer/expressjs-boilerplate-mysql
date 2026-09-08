70d24b8f68428d51832a2b8299ddef066070e9b7
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-09-08 14:57:53 +0000

    refactor(routes): split account and user routes into modules

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -3,21 +3,20 @@
 import {Router} from "express";
 import {AuthenticationMiddleware} from "../common/middleware/authentication.middleware";
 import {AuthorizationMiddleware} from "../common/middleware/authorization.middleware";
-import {TwoFactorAuthenticationMiddleware} from "../common/middleware/two-factor-authentication.middleware";
-import AccountController from "../modules/users/controllers/account.controller";
 import SettingController from "../modules/system/controllers/setting.controller";
 import RoleController from "../modules/role/role.controller";
-import UserController from "../modules/users/controllers/user.controller";
 import RouteGuardController from "../modules/system/controllers/route-guard.controller";
+
 import authRoutes from "../modules/auth/routes"
+import accountRoutes from "../modules/users/account.routes"
+import userRoutes from "../modules/users/user.routes";
 
 export default () => {
     const router = Router();
 
     router.use("/auth", authRoutes());
-
-    router.put("/account/information", [AuthenticationMiddleware(), TwoFactorAuthenticationMiddleware()], AccountController.information);
-    router.put("/account/password", [AuthenticationMiddleware(), TwoFactorAuthenticationMiddleware()], AccountController.password);
+    router.use("/account", accountRoutes());
+    router.use("/users", userRoutes());
 
     router.get("/settings", [AuthenticationMiddleware(), AuthorizationMiddleware("settings:browse")], SettingController.browse);
     router.get("/settings/values", [AuthenticationMiddleware()], SettingController.values);
@@ -36,12 +35,6 @@ export default () => {
     router.get("/route/guards/:id", [AuthenticationMiddleware(), AuthorizationMiddleware("route-guards:view")], RouteGuardController.view);
     router.post("/route/guards", [AuthenticationMiddleware(), AuthorizationMiddleware("route-guards:create")], RouteGuardController.create);
     router.delete("/route/guards/:id", [AuthenticationMiddleware(), AuthorizationMiddleware("route-guards:delete")], RouteGuardController.delete);
-
-    router.get("/users", [AuthenticationMiddleware(), AuthorizationMiddleware("users:browse")], UserController.browse);
-    router.get("/users/:id", [AuthenticationMiddleware(), AuthorizationMiddleware("users:view")], UserController.view);
-    router.post("/users", [AuthenticationMiddleware(), AuthorizationMiddleware("users:create")], UserController.create);
-    router.put("/users/:id", [AuthenticationMiddleware(), AuthorizationMiddleware("users:update")], UserController.update);
-    router.delete("/users/:id", [AuthenticationMiddleware(), AuthorizationMiddleware("users:delete")], UserController.delete);
 
     return router;
 }
