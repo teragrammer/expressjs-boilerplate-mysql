34b253d532b3ba213f72569f1e969458de250dab
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-09-10 14:44:41 +0000

    refactor user routes with dependency injection and validation

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -1,16 +1,52 @@
+// src/modules/users/user.routes.ts
 import {Router} from "express";
 import {AuthenticationMiddleware} from "../../common/middleware/authentication.middleware";
 import {AuthorizationMiddleware} from "../../common/middleware/authorization.middleware";
-import UserController from "./controllers/user.controller";
+import {validate} from "../../common/middleware/validate.middleware";
+import {userCreateSchema} from "./validations/user-create.schema";
+import {userUpdateSchema} from "./validations/user-update.schema";
+import {UserController} from "./controllers/user.controller";
+import {UserService} from "./services/user.service";
+import {UserRepository} from "./user.repository";
+import {SecurityUtil} from "../../common/utils/security.util";
+import {__ENV} from "../../config/environment";
+
+const userController = new UserController(new UserService(
+    new UserRepository(),
+    new SecurityUtil({
+        bcryptSecret: __ENV.BCRYPT_SECRET,
+        bcryptSaltRounds: Number(__ENV.BCRYPT_SALT_ROUND || 10),
+    })
+));
 
 export default () => {
     const router = Router();
 
-    router.get("/users", [AuthenticationMiddleware(), AuthorizationMiddleware("users:browse")], UserController.browse);
-    router.get("/users/:id", [AuthenticationMiddleware(), AuthorizationMiddleware("users:view")], UserController.view);
-    router.post("/users", [AuthenticationMiddleware(), AuthorizationMiddleware("users:create")], UserController.create);
-    router.put("/users/:id", [AuthenticationMiddleware(), AuthorizationMiddleware("users:update")], UserController.update);
-    router.delete("/users/:id", [AuthenticationMiddleware(), AuthorizationMiddleware("users:delete")], UserController.delete);
+    router.post("/", [
+        AuthenticationMiddleware(),
+        AuthorizationMiddleware("users:create"),
+        validate(userCreateSchema, [
+            "first_name", "middle_name", "last_name",
+            "role_id", "phone", "email", "username", "password",
+            "address", "comments", "status",
+        ])
+    ], userController.create);
+
+    router.put("/:id", [
+        AuthenticationMiddleware(),
+        AuthorizationMiddleware("users:update"),
+        validate((req) => userUpdateSchema(req.credentials.jwt.uid), [
+            "first_name", "middle_name", "last_name",
+            "role_id", "phone", "email", "username", "password",
+            "address", "comments", "status",
+        ])
+    ], userController.update);
+
+    router.get("/", [AuthenticationMiddleware(), AuthorizationMiddleware("users:browse")], userController.browse);
+
+    router.get("/:id", [AuthenticationMiddleware(), AuthorizationMiddleware("users:view")], userController.view);
+
+    router.delete("/:id", [AuthenticationMiddleware(), AuthorizationMiddleware("users:delete")], userController.delete);
 
     return router;
 }
