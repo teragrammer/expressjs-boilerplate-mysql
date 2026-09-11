3f93927ddc2ba9ed4edd34b821d8621761deb0b4
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-09-11 16:40:14 +0000

    refactor settings migration schema

--- database/migrations/20250313141523_settings.ts
+++ database/migrations/20250313141523_settings.ts
@@ -1,25 +1,49 @@
-import type {Knex} from "knex";
-import {DATA_TYPES} from "../../src/modules/system/models/setting.model";
+// database/migrations/20250313141523_settings.ts
+import type { Knex } from "knex";
+import { DATA_TYPES } from "../../src/modules/system/models/setting.model";
 
 export async function up(knex: Knex): Promise<void> {
-    return knex.schema.createTable('settings', table => {
-        table.increments('id').primary();
+    return knex.schema.createTable("settings", table => {
+        table.increments("id").primary();
 
-        table.string('name', 100).unique().notNullable();
-        table.string('slug', 100).unique().index().notNullable();
-        table.text('value').nullable();
-        table.text('description', 'tinytext').nullable();
-        table.enum('type', DATA_TYPES).defaultTo('string');
+        table.string("name", 100).unique().notNullable();
 
-        table.boolean('is_disabled').notNullable().defaultTo(0);
-        table.boolean('is_public').notNullable().defaultTo(1);
+        table
+            .string("slug", 100)
+            .unique()
+            .notNullable();
 
-        table.dateTime('created_at').index().defaultTo(knex.fn.now()).nullable();
-        table.dateTime('updated_at').defaultTo(knex.fn.now()).nullable();
+        table.text("value").nullable();
+
+        table.text("description").nullable();
+
+        table
+            .enum("type", DATA_TYPES)
+            .notNullable()
+            .defaultTo("string");
+
+        table
+            .boolean("is_disabled")
+            .notNullable()
+            .defaultTo(false);
+
+        table
+            .boolean("is_public")
+            .notNullable()
+            .defaultTo(true);
+
+        table
+            .dateTime("created_at")
+            .notNullable()
+            .defaultTo(knex.fn.now());
+
+        table
+            .dateTime("updated_at")
+            .notNullable()
+            .defaultTo(knex.fn.now());
     });
 }
 
 export async function down(knex: Knex): Promise<void> {
-    return knex.schema.dropTable('settings');
+    return knex.schema.dropTable("settings");
 }
-
