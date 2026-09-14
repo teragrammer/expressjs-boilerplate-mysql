93777c0df403681ccd54de8974a7192ec71a5383
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-09-14 15:27:30 +0000

    feat(users): add user update validation schema

---
+++ src/modules/users/validations/user-update.schema.ts
@@ -0,0 +1,35 @@
+// src/modules/users/validations/user-update.schema.ts
+import Joi from '../../../shared/validations/joi';
+import {validateCompositeUnique} from "../../../shared/validations/database/unique";
+import {STATUSES} from "../user.interface";
+
+export const userUpdateSchema = (userId: string | number) => Joi.object({
+    first_name: Joi.string().min(1).max(100).required(),
+    middle_name: Joi.string().min(1).max(100).allow(null, ""),
+    last_name: Joi.string().min(1).max(100).required(),
+
+    address: Joi.string().max(100).allow(null, ""),
+    comments: Joi.string().max(100).allow(null, ""),
+    status: Joi.string().valid(...STATUSES).allow(null, ""),
+
+    role_id: Joi.number().integer().required()
+        .external(validateCompositeUnique("roles", ["role_id"], {ignoreId: userId})),
+    phone: Joi.string().min(10).max(16).allow(null, "")
+        .external(validateCompositeUnique("users", ["phone"], {ignoreId: userId})),
+    email: Joi.string().email().max(180).allow(null, "")
+        .external(validateCompositeUnique("users", ["email"], {ignoreId: userId})),
+    username: Joi.string().min(2).max(16).pattern(/^[a-zA-Z0-9_]+$/).allow(null, "")
+        .external(validateCompositeUnique("users", ["username"], {ignoreId: userId}))
+        .required(),
+    password: Joi.string().min(8).max(32)
+        .pattern(/[A-Z]/)           // At least one uppercase letter
+        .pattern(/[a-z]/)           // At least one lowercase letter
+        .pattern(/[0-9]/)           // At least one number
+        .pattern(/[^A-Za-z0-9]/)    // At least one special character (e.g., !, @, #)
+        .messages({
+            "string.min": "Password should be at least 8 characters long.",
+            "string.max": "Password should be no longer than 32 characters.",
+            "string.pattern.base": "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.",
+        })
+        .allow(null, ""),
+});
