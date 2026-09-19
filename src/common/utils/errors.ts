c6106308a68bf7faf82852455e9ddb1392eeedb3
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-09-19 15:34:16 +0000

    refactor: simplify AppError metadata handling

--- src/common/utils/errors.ts
+++ src/common/utils/errors.ts
@@ -1,4 +1,5 @@
 // src/common/utils/errors.ts
+import Messages, {MessageKey} from "./messages";
 
 /**
  * A reusable custom error class for clean, scalable Express error handling.
@@ -8,15 +9,12 @@ export class AppError extends Error {
     public errorCode: string;
     public statusCode: number;
 
-    constructor(message: string, errorCode: string, statusCode: number) {
-        super(message);
-        this.errorCode = errorCode;
-        this.statusCode = statusCode;
+    constructor(errMeta: typeof Messages[MessageKey], customMessage?: string) {
+        super(customMessage ?? errMeta.message);
+        this.errorCode = errMeta.code;
+        this.statusCode = errMeta.status;
 
-        // Restores proper prototype chain for ES5 inheritance
         Object.setPrototypeOf(this, new.target.prototype);
-
-        // Captures the call stack trace, omitting this constructor call
         Error.captureStackTrace(this, this.constructor);
     }
 }
