240e1d6414bb1a29a6e14580455d013459663527
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-09-21 09:23:19 +0000

    test: configure Vitest for source and integration tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ export default defineConfig({
     test: {
         globals: true,
         // Scan only files inside any __test__ folder ending with .test.ts or .spec.ts
-        include: ['**/__test__/**/*.{test,spec}.ts'],
+        include: [
+            '**/src/**/*.{test,spec}.ts',
+            'tests/integration/**/*.{spec,test}.ts'
+        ],
+        setupFiles: ['./tests/integration/setup.ts'],
     },
 });
