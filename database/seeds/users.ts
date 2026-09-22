0cd5aff7e42fd42c7275b641b8c5a25c30760caf
teragrammer <208279979+teragrammer@users.noreply.github.com>
2026-09-22 15:41:42 +0000

    fix: update user seed to use security container and role IDs

--- database/seeds/users.ts
+++ database/seeds/users.ts
@@ -1,17 +1,26 @@
 import {Knex} from "knex";
-import {SecurityUtil} from "../../src/common/utils/security.util";
+import {securityUtil} from "../../src/config/container";
 
 export async function seed(knex: Knex): Promise<void> {
     // Insert roles
-    const [ADMIN_ROLE] = await knex("roles").insert({name: 'Administrator', slug: 'admin', is_public: 0, is_bypass_authorization: 1}).returning('id');
+    const [ADMIN_ROLE] = await knex("roles").insert({
+        name: 'Administrator',
+        slug: 'admin',
+        is_public: 0,
+        is_bypass_authorization: 1
+    }).returning('id');
     const [MANAGER_ROLE] = await knex("roles").insert({name: 'Manager', slug: 'manager', is_public: 0}).returning('id');
-    const [CUSTOMER_ROLE] = await knex("roles").insert({name: 'Customer', slug: 'customer', is_public: 0}).returning('id');
+    const [CUSTOMER_ROLE] = await knex("roles").insert({
+        name: 'Customer',
+        slug: 'customer',
+        is_public: 0
+    }).returning('id');
 
     // Inserts users
-    const PASSWORD = await SecurityUtil().hash("123456");
+    const password = await securityUtil.hash("123456");
     await knex("users").insert([
-        {username: "admin", PASSWORD, role_id: ADMIN_ROLE},
-        {username: "manager", PASSWORD, role_id: MANAGER_ROLE},
-        {username: "customer", PASSWORD, role_id: CUSTOMER_ROLE},
+        {username: "admin", password: password, role_id: ADMIN_ROLE.id},
+        {username: "manager", password: password, role_id: MANAGER_ROLE.id},
+        {username: "customer", password: password, role_id: CUSTOMER_ROLE.id},
     ]);
 }
